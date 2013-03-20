acf348b133fbaeaa420ef3dff59ff153af58a803
pingwan <pingw@live.nl>
2013-03-20 20:06:27 +0100

    tag mouseover FIXED!  + plaatjes onder gefixed.

--- Prototype2/javascript/main.js
+++ Prototype2/javascript/main.js
@@ -3,24 +3,22 @@ $(document).ready(function(){
 //document.write('blazz');
 
 //############# Tags onder timeline ################# 
-			$(".icon-fire").on("mouseenter",function(){
+				$(".icon-fire").on("mouseenter",function(){
 				updateExtraInfo($(this).css("margin-left"));
+				$("#extrainfo_inner").css("display", "block");	
 			});
 
 			$(".icon-fire").on("mouseout",function(){
-				$("#extrainfo_inner").innerHTML="HHH";
-				$("#extrainfo_inner").fadeOut();			
+				$("#extrainfo_inner").html("");
+						
 			});
 
 //############# Update extra info ###################
 function updateExtraInfo(starttime){
-
-
-
-		var canvas = $("#extrainfo_inner");
-		canvas.append("hoi");
-		canvas.fadeIn();
-	}
+		$("#extrainfo_inner").append('<img src=http://placehold.it/350x150><br/>')
+								.append('at ' + parseInt(starttime) + 'seconden<br/>')
+								.append('User rating');
+	}		
 
 });
 
