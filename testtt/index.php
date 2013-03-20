86946b2cc9998a383836fbc40e66eef32f1b4c9d
pingwan <pingw@live.nl>
2013-03-20 14:12:39 +0100

    change password tagcllpud

--- testtt/index.php
+++ testtt/index.php
@@ -78,7 +78,7 @@
 	</header>
 
 	<?php
-	$link = mysql_connect('localhost','root','');
+	$link = mysql_connect('localhost','root','root');
 	if (!$link) {
 	    die('Could not connect: ' . mysql_error());
 	}
