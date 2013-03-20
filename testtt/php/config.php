6ae6a55fc1498aea035a169d80373de6e3dc6e76
pingwan <pingw@live.nl>
2013-03-20 13:43:34 +0100

    php config file
    Omdat iedereen een aparte localhost heeft en dus een andere username en
    ww. Edit deze database naar jouw eigen login gevens.

---
+++ testtt/php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	##### this file is used to log in the database ######
+
+	$con = mysql_connect("localhost", "root", "root");
+
+	if(!$con)
+	{
+		echo mysql_error();
+	}
+	else
+	{
+		mysql_select_db("user_database", $con);	
+	}
+?>
