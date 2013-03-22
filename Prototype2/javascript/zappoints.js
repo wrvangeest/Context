2833fef977a5b74f2d8a540b77482f0105c50bde
wrvangeest <wiebevangeest@hotmail.com>
2013-03-22 15:08:43 +0100

    Seperate javascript file for zappoints.

---
+++ Prototype2/javascript/zappoints.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+//############# Tags onder timeline ################# 
+				$(".icon-fire").on("mouseenter",function(){
+				updateExtraInfo($(this).css("margin-left"));
+				$("#extrainfo_inner").css("display", "block");	
+			});
+
+			$(".icon-fire").on("mouseout",function(){
+				$("#extrainfo_inner").html("");
+						
+			});
+
+//############# Update extra info ###################
+function updateExtraInfo(starttime){
+		$("#extrainfo_inner").append('<img src=http://placehold.it/350x150><br/>')
+								.append('at ' + parseInt(starttime) + 'seconden<br/>')
+								.append('User rating');
+	}		
+
+});
