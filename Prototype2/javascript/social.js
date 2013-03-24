77aa76754e6434db45684255718fa2770abedfed
wrvangeest <wiebevangeest@hotmail.com>
2013-03-24 17:05:29 +0100

    Tweet button now working
    Tweet button now works, message prepared for tweet can be dynamically
    set in social.js.

--- Prototype2/javascript/social.js
+++ Prototype2/javascript/social.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
 
-alert("Social working!");
+//alert("Social working!");
+
+createTwit();
+//getScript();
+
 //Twitter implementation
-var div = document.getElementById("social");
-var twit = document.createElement('a');
-twit		.href = "https://twitter.com/share";
-			.class = "twitter-share-button";
-div.appendChild(twit);
+function createTwit() {
 
-//<a href="https://twitter.com/share" class="twitter-share-button" data-url="localhost" data-text="Check SocialZap!" data-hashtags="SocialZap">Tweet</a>
-//<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
+
+
+	$('#twitter_link').each(function(){
+		$(this).attr('data-url', "localhost");
+		$(this).attr('data-hashtags',"SocialZap");
+		$(this).attr('data-text', "Tweet over dit tijdstip: " +  "!")
+	});
+	getScript();
+}
+
+function getScript() {
+	$.getScript('http://platform.twitter.com/widgets.js');
+}
+
+alert("Social still working!");
 });
