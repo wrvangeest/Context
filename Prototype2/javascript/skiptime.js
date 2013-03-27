c2177bcafb628ce7f94be1d222858292fbe5f351
pingwan <pingw@live.nl>
2013-03-27 12:13:17 +0100

    it now also plays the video with time in URL!!

--- Prototype2/javascript/skiptime.js
+++ Prototype2/javascript/skiptime.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	function getSkipTime(){
 		var hash = getUrlVars();
 		//alert(typeof parseInt(hash['time']));
-		Popcorn("#video").currentTime(hash['time']);
+		Popcorn("#video").currentTime(hash['time']).play();
 	}
 	
 	//Calls function after video is loaded
