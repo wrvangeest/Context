9748260be218278b4b28ce8e9432cb532d805114
wrvangeest <wiebevangeest@hotmail.com>
2013-04-25 11:15:03 +0200

    Updated test function a little bit.

--- Prototype3/javascript/test/spec/JSSpec.js
+++ Prototype3/javascript/test/spec/JSSpec.js
@@ -5,13 +5,12 @@ describe("Javascript functions", function () {
 
 	describe("Misc functions", function () {
 		it("checkTime should stop", function() {
-			var cb = checkTime(function(dur) { return dur }, 500);
-			expect(cb).toEqual(-1);
+			
 		});
 
 	});
 
 	describe("Tag functions", function () {
-
+		it("")
 	});
 });
