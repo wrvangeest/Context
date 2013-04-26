92dd68cbeb3e2f76229033a6633e1229d57d452b
Anish Narwade <anish.narwade@gmail.com>
2013-04-26 19:48:34 +0200

    frame previews

--- Prototype3/index.php
+++ Prototype3/index.php
@@ -193,7 +193,6 @@
 	
 	<script src ="javascript/main.js"></script>
 	<script src ="javascript/video.js"></script>
-		<script src ="simple_tip.js"></script>
 	<script src ="javascript/tagdata.js"></script>
 	<script src ="javascript/social.js"></script>
 	<script src ="javascript/checktime.js"></script>
