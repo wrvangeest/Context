9d860d1e21e67a68b1125aec673d827f38c07d72
pingwan <pingw@live.nl>
2013-05-03 10:52:24 +0200

    Added browse page
    still working on it….

---
+++ Prototype4/browse.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>SocialZap</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		<script src ="javascript/bootstrap.js"></script>
+		<script src ="javascript/jqcloud-1.0.3.js"></script>
+		<script src ="http://popcornjs.org/code/dist/popcorn.min.js"></script>
+		<!--  css files -->
+		<LINK rel="stylesheet" href="css/style.css" type="text/css" MEDIA=screen>
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+		<link rel="stylesheet" href="css/jqcloud.css" type="text/css">
+		<link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+		
+	</head>
+	
+	<body>
+	
+	
+
+		<!--page wrapper -->
+		<div class="main-content-wrapper">
+			
+			<!-- header -->
+			
+			<header class="header2">
+					<div class="container">
+						<h1 class="logo">
+							<a href="index.php">
+								<img src="logo2.jpg">
+							</a>
+
+						</h1>
+						
+				<div class="row">
+					
+
+					<div class="span7" style="float:right; height:66px;">
+						<ul class="navigation">
+			     			<li><a href="index.html">Home</a></li>
+			     			<li><a href="#">Browse</a></li>
+			     			<li><a href="#">Random Video</a></li>
+			     			<li><a href="#">Sign Up</a></li>
+			     			<li><a href="#">Login</a></li>
+			    		</ul>
+
+					</div>
+
+				</div>
+				</div>
+			<!-- end header -->
+			</header>
+
+
+			<div class="container">
+				<div class="row">
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>		
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>	
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>
+					<div class="span4" style="background-color:red;">
+						<img src="http://placehold.it/350x150"/>
+					</div>
+				</div>
+			</div
+
+
+		<!-- end page wrapper -->
+		</div>
+
+	
+	<script src ="javascript/main.js"></script>
+	<script src ="javascript/video.js"></script>
+	<script src ="simple_tip.js"></script>
+	<script src ="javascript/tagdata.js"></script>
+	<script src ="javascript/social.js"></script>
+	<script src ="javascript/checktime.js"></script>
+	<script src ="javascript/geturlvars.js"></script>
+	<script src ="javascript/skiptime.js"></script>
+	</body>
+	</html>
