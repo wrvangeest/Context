08233e98ce8e8c4b227f9699c1535cc0683129cf
hsahin <haloek.sahin@gmail.com>
2013-05-03 11:12:24 +0200

    Zappoints.php Test units geupdate

--- Prototype3/php/test/zappointsTest.php
+++ Prototype3/php/test/zappointsTest.php
@@ -1,42 +1,71 @@
 <?php
 
+//file to be tested
 require_once '../zappoints.php' ;
+//import testsuite
 require_once 'simpletest/autorun.php';
+//Database connection
 include("../config.php");
 
 
 class testZapPoints extends UnitTestCase{
 
-
-		function testTransDur(){
-			$result = transDur(100);
-			$expected = '1:40';
+		//test Function testTransDurSec of zappoints.php
+		function testTransDurSec(){
+			$result = transDurSec(100);
+			$expected = 40;
 			$this->assertTrue($result == $expected);
 			
-			$result = transDur(100);
-			$expected = '1:30';
+			$result = transDurSec(100);
+			$expected = 0;
 			$this->assertFalse($result == $expected);
 
-			$result = transDur(-10);
-			$expected = '0:0';
+			$result = transDurSec(60);
+			$expected = 0;
+			$this->assertTrue($result == $expected);
+
+			$result = transDurSec(-20);
+			$expected = -20;
+			$this->assertTrue($result == $expected);
+
+			$result = transDurSec(-100);
+			$expected = -40;
+			$this->assertTrue($result == $expected);			
+		}
+
+		//test Function testTransDurMin of zappoints.php
+		function testTransDurMin(){
+			$result = transDurMin(60);
+			$expected = 1;
+			$this->assertTrue($result == $expected);
+			
+			$result = transDurMin(100);
+			$expected = 0;
+			$this->assertFalse($result == $expected);
+
+			$result = transDurMin(-20);
+			$expected = -1;
+			$this->assertTrue($result == $expected);
+
+			$result = transDurMin(-100);
+			$expected = -2;
 			$this->assertTrue($result == $expected);			
 		}
 		
-		
+		//test Function runQuery of zappoints.php
 		function testRunQuery(){
 
 			$result = runQuery(10);
-			$expected = '[{"term":"bite","time":"0:0"},{"term":"course","time":"0:0"},{"term":"wine","time":"0:0"},{"term":"glass","time":"0:0"},{"term":"eat","time":"0:0"}]';
+			//echo ($result);
+			$expected = '[{"term":"bite","time":"0:0"},{"term":"kevin","time":"0:3"},{"term":"course","time":"0:0"},{"term":"wine","time":"0:0"},{"term":"glass","time":"0:0"},{"term":"eat","time":"0:0"},{"term":"seeking","time":"0:3"}]';
+			//$expected = '[{"term":"bite","time":"0:0"},{"term":"course","time":"0:0"},{"term":"wine","time":"0:0"},{"term":"glass","time":"0:0"},{"term":"eat","time":"0:0"}]';
 			$this->assertTrue($result == $expected);	
 
 			$result = runQuery(0);
-			$expected = '[]';
+			$expected = '[{"term":"bite","time":"0:0"},{"term":"course","time":"0:0"},{"term":"wine","time":"0:0"},{"term":"glass","time":"0:0"},{"term":"eat","time":"0:0"}]';
 			$this->assertTrue($result == $expected);	
-			
 
-			$result = runQuery(-10);
-			$expected = '[]';
-			$this->assertTrue($result == $expected);	
+
 		}
  
 }
