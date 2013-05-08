2fe17ad5260f2d7c37e1f06bb4a4893fa50c5fe2
pingwan <pingw@live.nl>
2013-05-08 23:51:22 +0200

    added extra information in de link when clicking on a video from browse page.

--- Prototype4/javascript/skiptime.js
+++ Prototype4/javascript/skiptime.js
@@ -1,9 +1,13 @@
 //########### Set video to time in url (if any) ################
 function getSkipTime(){
 	var hash = getUrlVars();
+
+	$("#videosource").attr("src","videos/" + hash['videoname'] + ".mp4");
+
 	if(!isNaN(hash['time'])){
 		Popcorn("#video").currentTime(hash['time']).play();
 	}
 
 	console.log(hash['videoid']);
+	console.log(hash['videoname']);
 }
