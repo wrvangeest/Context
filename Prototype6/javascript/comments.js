f743564823ef1cc231f7acbfd8cce330d870457b
hsahin <haloek.sahin@gmail.com>
2013-06-09 13:47:50 +0200

    comments met tijd , mooie format

--- Prototype6/javascript/comments.js
+++ Prototype6/javascript/comments.js
@@ -22,11 +22,75 @@ function loadComments(){
 		})
 
 	setTimeout(function(){
-	console.log(comments);
 	jQuery.each(comments[0], function(index,item) {
 
-		$('.comments').append('<div class="a-comment"><div class="user-photo"><li class="icon-user icon-2x"></li></div><div class="comment-info">id:'+item.id+' at 0:25, 1 hours ago:</div><div class="comment-text"> '+item.text+' </div></div>')
-	})
-	},3000);
+		//make a new date object,
+		//get the right format of date and time
+		//and how long ago it was posted
+		var datumpost = new Date(item.datum);
+		var datumformat = getPostTime(datumpost);
+		var geleden = getPostPast(datumpost);
 
+
+		//create the elements and append it to comments div
+		var acomment = $(document.createElement('div'));
+		acomment.addClass("a-comment");
+
+		var userphoto = $(document.createElement('div'));
+		userphoto.addClass("user-photo");
+		userphoto.html('<li class="icon-user icon-2x"></li>');
+			
+		var commentinfo = $(document.createElement('div'));
+		commentinfo.addClass("comment-info");
+		commentinfo.html('naam: '+ item.id + ' ' + datumformat + ' , ' + geleden);
+
+		var commenttext = $(document.createElement('div'));
+		commenttext.addClass("comment-text");
+		commenttext.html(item.text);
+
+		$(acomment).append(userphoto)
+				   .append(commentinfo)
+				   .append(commenttext)
+
+		$('.comments').append(acomment)
+	})
+	},1500);
+
+}
+
+//return the date in a clearer format
+function getPostTime(datumpost){
+					jaar = datumpost.getFullYear();
+					maand = datumpost.getMonth();
+					dag = datumpost.getDate();
+					uur = datumpost.getHours();
+					minuut = datumpost.getMinutes();
+					sec = datumpost.getSeconds();
+
+					var datumformat = dag+"/"+maand+"/"+jaar+" around "+uur+":"+minuut;
+					return datumformat;
+}
+
+//returns the differance in time with now and the given date: datumpost
+// extends with the correct extension
+function getPostPast(datumpost){
+
+						var nu = new Date()
+						var verschildatum = nu - datumpost;
+						
+						if(verschildatum<1000){
+						    return Math.floor(verschildatum/1000) + " seconds ago";
+						}
+						if(verschildatum<(60*60*1000)){
+						    return Math.floor(verschildatum/(60*1000)) +" minutes ago";
+						}
+						if(verschildatum<(24*60*60*1000)){
+							return Math.floor(verschildatum/(60*60*1000)) +" hours ago";
+						}
+						if(verschildatum<(365*24*60*60*1000)){
+							return Math.floor(verschildatum/(24*60*60*1000))+" days ago";
+						}
+						else{
+						  	return "once upon a time";
+						}
 }
