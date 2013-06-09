0487174880a91bed5f2ed95bedd1fe91645781cf
hsahin <haloek.sahin@gmail.com>
2013-06-09 14:12:15 +0200

    beter

--- Prototype6/javascript/comments.js
+++ Prototype6/javascript/comments.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
 	
 	loadComments();
 
-
-	//$('#comments-input')
-
 });
 
 
@@ -13,51 +10,49 @@ function loadComments(){
 
 	var hash = getUrlVars();
 	var vidid = hash['vidid'];
-	var comments = [];
 	
 	$.post("php/comments.php?vidid=" + vidid)
 		.done(function (data) {
 			console.log(data);
-			comments.push(JSON.parse(data));
+			putComments(JSON.parse(data));
 		})
-
-	setTimeout(function(){
-	jQuery.each(comments[0], function(index,item) {
-
-		//make a new date object,
-		//get the right format of date and time
-		//and how long ago it was posted
-		var datumpost = new Date(item.datum);
-		var datumformat = getPostTime(datumpost);
-		var geleden = getPostPast(datumpost);
-
-
-		//create the elements and append it to comments div
-		var acomment = $(document.createElement('div'));
-		acomment.addClass("a-comment");
-
-		var userphoto = $(document.createElement('div'));
-		userphoto.addClass("user-photo");
-		userphoto.html('<li class="icon-user icon-2x"></li>');
-			
-		var commentinfo = $(document.createElement('div'));
-		commentinfo.addClass("comment-info");
-		commentinfo.html('naam: '+ item.name + ' ' + datumformat + ' , ' + geleden);
-
-		var commenttext = $(document.createElement('div'));
-		commenttext.addClass("comment-text");
-		commenttext.html(item.text);
-
-		$(acomment).append(userphoto)
-				   .append(commentinfo)
-				   .append(commenttext)
-
-		$('.comments').append(acomment)
-	})
-	},1500);
 
 }
 
+function putComments(data){
+		jQuery.each(data, function(index,item) {
+
+			//make a new date object,
+			//get the right format of date and time
+			//and how long ago it was posted
+			var datumpost = new Date(item.datum);
+			var datumformat = getPostTime(datumpost);
+			var geleden = getPostPast(datumpost);
+
+
+			//create the elements and append it to comments div
+			var acomment = $(document.createElement('div'));
+			acomment.addClass("a-comment");
+
+			var userphoto = $(document.createElement('div'));
+			userphoto.addClass("user-photo");
+			userphoto.html('<li class="icon-user icon-2x"></li>');
+				
+			var commentinfo = $(document.createElement('div'));
+			commentinfo.addClass("comment-info");
+			commentinfo.html(item.name + ' @ ' + datumformat + ' , ' + geleden);
+
+			var commenttext = $(document.createElement('div'));
+			commenttext.addClass("comment-text");
+			commenttext.html(item.text);
+
+			$(acomment).append(userphoto)
+					   .append(commentinfo)
+					   .append(commenttext)
+
+			$('.comments').append(acomment)
+		})
+}
 //return the date in a clearer format
 function getPostTime(datumpost){
 					jaar = datumpost.getFullYear();
@@ -66,7 +61,9 @@ function getPostTime(datumpost){
 					uur = datumpost.getHours();
 					minuut = datumpost.getMinutes();
 					sec = datumpost.getSeconds();
-
+					if(minuut <10){
+						minuut = '0'+minuut;
+					}
 					var datumformat = dag+"/"+maand+"/"+jaar+" around "+uur+":"+minuut;
 					return datumformat;
 }
