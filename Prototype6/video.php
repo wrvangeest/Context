9fdebfd81c7a45664b5d84b09384e1403cc94d4e
wrvangeest <wiebevangeest@hotmail.com>
2013-06-17 10:12:41 +0200

    Added icons

--- Prototype6/video.php
+++ Prototype6/video.php
@@ -24,6 +24,7 @@
 		<LINK rel="stylesheet" href="css/tagtoggler.css" type="text/css" >
 		<LINK rel="stylesheet" href="css/ratingbar.css" type="text/css" >	
 		<LINK rel="stylesheet" href="css/style.css" type="text/css" MEDIA=screen>
+		<link rel="shortcut icon" href="img/bolt.ico">
 		
 	</head>	
 	<body>
